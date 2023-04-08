75fb95265ab6fd9ffedf2431df60a772d3c6c8b0
Raulgaray <85389355+Raulgaray@users.noreply.github.com>
2023-04-08 01:16:15 -0500

    Agregar archivos de proyecto.

---
+++ Lab02/Program.cs
@@ -0,0 +1,9 @@
+﻿
+Producto producto = new Producto();
+producto.Nombre = "Producto 1";
+producto.Precio = 30.0;
+producto.FechaVencimiento = new DateTime(2023, 04, 10);
+producto.Descripcion = "Descripción del producto 1";
+
+producto.EstaVencido();
+Console.WriteLine("Precio de venta: " + producto.GetPrecioVenta());
